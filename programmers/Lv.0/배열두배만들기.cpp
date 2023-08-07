1e45dcea1858be56e51b552f7e392a8ba9d77d6d
Seungeun Lee <67785484+seungeunlee00@users.noreply.github.com>
2023-08-07 17:35:52 +0900

    배열두배만들기

---
+++ programmers/Lv.0/배열두배만들기.cpp
@@ -0,0 +1,18 @@
+/*
+  문제 설명
+  정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for(int i=0; i<numbers.size(); i++){
+        numbers[i]=numbers[i]*2;
+    }
+    answer=numbers;
+    return answer;
+}
