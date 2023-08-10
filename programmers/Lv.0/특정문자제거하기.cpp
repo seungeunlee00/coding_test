0ae597e825426f2f17c2b35c82ddbcfaff31c8a3
seungeunlee00 <67785484+seungeunlee00@users.noreply.github.com>
2023-08-10 17:07:45 +0900

    특정문자제거하기

---
+++ programmers/Lv.0/특정문자제거하기.cpp
@@ -0,0 +1,17 @@
+/*
+  문제 설명
+  문자열 my_string과 문자 letter이 매개변수로 주어집니다.'
+  my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string letter) {
+    string answer = "";
+    for(int i=0; i<my_string.length(); i++)
+        if(my_string[i]!=letter[0]) answer+=my_string[i];
+    return answer;
+}
