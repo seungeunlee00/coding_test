79eaae0ca729fd532093e45c326b51cb17e30f3a
Seungeun Lee <67785484+seungeunn@users.noreply.github.com>
2023-09-15 16:37:45 +0900

    분수의덧셈

--- programmers/Lv.0/분수의덧셈.cpp
+++ programmers/Lv.0/분수의덧셈.cpp
@@ -14,7 +14,7 @@ vector<int> solution(int numer1, int denom1, int numer2, int denom2) {
     int numer = (denom1 * numer2) + (denom2 * numer1); // numer:분자
     int denom = denom1 * denom2; // denom:분모
     
-    for(int i=2; i<min(denom, numer); i++){ // 최대공약수 구하기
+    for(int i=2; i<=min(denom, numer); i++){ // 최대공약수 구하기
         while(denom%i==0 && numer%i==0){
             denom=denom/i;
             numer=numer/i;            
